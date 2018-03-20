3c2b0a4307689e873a788fa8927aa2f085d4441d
2SMILE2 <alwayssmile11a1@gmail.com>
2018-03-20 16:25:53 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+</<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Clover Shop</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="extensions/css/bootstrap.min.css" />
+    <script src="extensions/js/jquery.min.js"></script>
+    <script src="extensions/js/bootstrap.min.js"></script>
+    <script src="main.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Clover Shop</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Product</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                <form class="form-inline ml-3 my-lg-0">
+                    <input style="width:400px" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </ul>
+            <ul class="navbar-nav mr-0">
+                <li class="nav-item">
+                    <a class="nav-link"  href="#"> <span class = "glyphicon glyphicon-shopping-cart"></span>Cart</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SignIn</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SignUp</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+</body>
+
+</html>
